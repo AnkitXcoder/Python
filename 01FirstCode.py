b73fd46bea0f0ca5671f4de4db00bbfc4ece2df3
AnkitX <ankitone02@gmail.com>
2025-05-25 10:20:32 +0530

    FC- adding first file

---
+++ 01FirstCode.py
@@ -0,0 +1,21 @@
+print("Ankit is a good coder \n You Know ??")   #BackSlash n --> nextLine
+print("Hello world")
+
+'''
+comment done .
+alt+downArrow
+'''             
+
+"""
+hkjsda
+jkhsd
+"""
+
+#Escape Sequence Character ESC 
+# \n is a escape Sequence Character
+#doble Quote ESC  \"   \"  --> provide character in ""
+
+print("\"ANkit\"\n Ram")
+
+print("ANkit",  2,3,4,5 , sep="-", end="009\n")
+print("ANkit")
