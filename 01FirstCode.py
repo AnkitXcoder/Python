a4110fab60037e5d61669b22f9236efa1d0c7f8f
AnkitX <ankitone02@gmail.com>
2025-05-25 10:32:48 +0530

    updating first commit

--- 01FirstCode.py
+++ 01FirstCode.py
@@ -15,7 +15,17 @@ jkhsd
 # \n is a escape Sequence Character
 #doble Quote ESC  \"   \"  --> provide character in ""
 
-print("\"ANkit\"\n Ram")
+print("\"Ankit\"\n Ram")
 
-print("ANkit",  2,3,4,5 , sep="-", end="009\n")
-print("ANkit")
+print("Ankit",  2,3,4,5 , sep="-", end="009\n")
+print("Ankit", 1,2,3,4,5,6, sep="--", end="end hua")
+
+# OUTPUT
+
+# Ankit is a good coder 
+#  You Know ??
+# Hello world
+# "Ankit"
+#  Ram
+# Ankit-2-3-4-5009
+# Ankit--1--2--3--4--5--6end hua
